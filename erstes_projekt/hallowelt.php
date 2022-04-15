336a69d88741f43489d67fd3a78621460acaf8f2
Bommberk <jim.mohncke@rshw.de>
2022-04-15 13:24:59 +0200

    test anrufen

---
+++ erstes_projekt/hallowelt.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Projekt</title>
+</head>
+<body>
+
+    <h1>hallo Welt</h1>
+
+    <p>was geht bei dir</p>
+
+    <?php
+        $name = "Jim";
+        echo "<p>Hallo ".$name." was geht?!</p>";
+
+        $zahl1 = 7;
+        $zahl2 = 8;
+        
+        echo "<p>7 * 8 = ".$zahl1 * $zahl2."</p>"
+    ?>
+
+    
+
+</body>
+</html>
