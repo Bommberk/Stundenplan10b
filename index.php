082236d17ed8fb55faf0f44950950c84884df0ac
Bommberk <83213983+Bommberk@users.noreply.github.com>
2022-06-02 14:21:37 +0200

    nochmal

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8" http-equiv="refresh" content="">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" >
     <title>Stundenplan9b</title>
-    <link rel="stylesheet" href="assets/style/style.css">
     <link rel="stylesheet" href="assets/style/header.css">
+    <link rel="stylesheet" href="assets/style/style.css">
 </head>
 <body>
 
