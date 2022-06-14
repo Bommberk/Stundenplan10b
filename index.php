109d8a28e2fbf95478d2c1a19d4d0a5978716fa9
Bommberk <83213983+Bommberk@users.noreply.github.com>
2022-06-14 11:22:05 +0200

    in rshw geändert

--- index.php
+++ index.php
@@ -252,7 +252,7 @@
 
     $empfaenger = "jim.mohncke@rshw.de";
 $betreff = "Die Mail-Funktion";
-$from = "From: Jim Mohncke <jim.mohncke@gmail.com>";
+$from = "From: Jim Mohncke <jim.mohncke@rshw.de>";
 $text = "Hallo Jim, \r\n irgend jemand hat deine Webseite: Stundenplan9b.ml besucht.!";
 
 mail($empfaenger, $betreff, $text, $from);
