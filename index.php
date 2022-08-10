2be36c74cb2a66322746059883bc338d9b8d97db
Bommberk <jim.mohncke@rshw.de>
2022-08-10 22:22:39 +0200

    text verändert

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@600&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
-<body style="background: #5c85dd;">
-    <h1>Es tut mir sehr leid Ihnen das mitteilen zu müssen, aber<br>
-        diese Webside ist jetzt durch große Probleme am Stundenplan geschlossen.<br>
+<body style="background: #5c85dd;font-family: 'Josefin Sans', sans-serif;">
+    <h1>Es tut mir sehr leid Ihnen mitteilen zu müssen, dass<br>
+        diese Webside jetzt durch große Probleme am Stundenplan geschlossen ist.<br>
         Vielen Dank für Ihr Ferständniss.
 </h1>
 </body>
