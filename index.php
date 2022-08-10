d37cc4b49a4b0b9dde8adc59971c7272c52b9ca4
Bommberk <jim.mohncke@rshw.de>
2022-08-10 22:12:14 +0200

    repeat aus

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body style="background: linear-gradient(to bottom,#07757d,#2495d6);">
+<body style="background: linear-gradient(to bottom,#07757d,#2495d6); background-repeat: none;">
     <h1>Es tut mir sehr leid Ihnen das mitteilen zu müssen, aber<br>
         diese Webside ist jetzt durch große Probleme am Stundenplan geschlossen.<br>
         Vielen Dank für Ihr Ferständniss.
