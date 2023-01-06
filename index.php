800935df792dd29ac8c89bd52113c5dd8c7517b0
Bommberk <jim.mohncke@rshw.de>
2023-01-06 13:53:43 +0100

    nochmal test hack

--- index.php
+++ index.php
@@ -70,6 +70,7 @@
             "2022-09-05 08:00:00" => "Mein Geburtstag",
             "2022-12-24 08:00:00" => "Weihnachten",
             "2023-01-01 00:00:00" => "Jahr 2023",
+            "2023-03-10 00:00:00" => "Osterferien",
             "2022-06-24 11:30:00" => "Sommerferien",
 
         );
@@ -134,7 +135,62 @@
          $datum['rest_minuten'] = floor($difference / $min);
         $difference = $difference - $datum['rest_minuten'] * $min;
 
-        echo "hallo";
+        include("stundenpläne/Medoo.php");
+
+        // Using Medoo namespace.
+    
+    use Medoo\Medoo;
+     
+    $database = new Medoo([
+        // [required]
+        'type' => 'mysql',
+        'host' => 'localhost',
+        'database' => 's5418_bencke',
+        'username' => 's5418_bencke',
+        'password' => '7sp4qM#86',
+     
+        // [optional]
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_general_ci',
+        'port' => 3306,
+     
+        // [optional] The table prefix. All table names will be prefixed as PREFIX_table.
+        'prefix' => '',
+     
+        // [optional] To enable logging. It is disabled by default for better performance.
+        'logging' => true,
+     
+        // [optional]
+        // Error mode
+        // Error handling strategies when the error is occurred.
+        // PDO::ERRMODE_SILENT (default) | PDO::ERRMODE_WARNING | PDO::ERRMODE_EXCEPTION
+        // Read more from https://www.php.net/manual/en/pdo.error-handling.php.
+        'error' => PDO::ERRMODE_SILENT,
+     
+        // [optional]
+        // The driver_option for connection.
+        // Read more from http://www.php.net/manual/en/pdo.setattribute.php.
+        'option' => [
+            PDO::ATTR_CASE => PDO::CASE_NATURAL
+        ],
+     
+        // [optional] Medoo will execute those commands after the database is connected.
+        'command' => [
+            'SET SQL_MODE=ANSI_QUOTES'
+        ]
+    ]);
+
+    $data = $database->select("logindaten",[
+        "benutzername",
+        "email",
+        "password",
+    ]);
+
+    $data = json_decode(json_encode($data));
+
+    echo "hallo";
+
+    print_r($data);
 
     ?>
     
