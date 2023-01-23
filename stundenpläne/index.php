72c348e1dc0ebda9bd68153361167c5dbdb252cf
Bommberk <jim.mohncke@rshw.de>
2023-01-23 15:22:33 +0100

    test6

--- stundenpläne/index.php
+++ stundenpläne/index.php
@@ -10,52 +10,54 @@
 <body>
     <?php
 
-        echo "hallo was geht bei dir!!!";
     
-    require("Medoo.php");
-      
-         
-    // Using Medoo namespace.
-    use Medoo\Medoo;
+require("Medoo.php");
+
+
+
+  
      
-    $database = new Medoo([
-        // [required]
-        'type' => 'mysql',
-        'host' => 'localhost',
-        'database' => 's5418_stundenplan',
-        'username' => 's5418_stundenplan',
-        'password' => 'R1?0vxx06',
-     
-        // [optional]
-        'charset' => 'utf8mb4',
-        'collation' => 'utf8mb4_general_ci',
-        'port' => 3306,
-     
-        // [optional] The table prefix. All table names will be prefixed as PREFIX_table.
-        'prefix' => '',
-     
-        // [optional] To enable logging. It is disabled by default for better performance.
-        'logging' => true,
-     
-        // [optional]
-        // Error mode
-        // Error handling strategies when the error is occurred.
-        // PDO::ERRMODE_SILENT (default) | PDO::ERRMODE_WARNING | PDO::ERRMODE_EXCEPTION
-        // Read more from https://www.php.net/manual/en/pdo.error-handling.php.
-        'error' => PDO::ERRMODE_SILENT,
-     
-        // [optional]
-        // The driver_option for connection.
-        // Read more from http://www.php.net/manual/en/pdo.setattribute.php.
-        'option' => [
-            PDO::ATTR_CASE => PDO::CASE_NATURAL
-        ],
-     
-        // [optional] Medoo will execute those commands after the database is connected.
-        'command' => [
-            'SET SQL_MODE=ANSI_QUOTES'
-        ]
-    ]);
+// Using Medoo namespace.
+use Medoo\Medoo;
+ 
+$database = new Medoo([
+    // [required]
+    'type' => 'mysql',
+    'host' => 'localhost',
+    'database' => 's5418_stundenplan',
+    'username' => 's5418_stundenplan',
+    'password' => 'R1?0vxx06',
+ 
+    // [optional]
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_general_ci',
+    'port' => 3306,
+ 
+    // [optional] The table prefix. All table names will be prefixed as PREFIX_table.
+    'prefix' => '',
+ 
+    // [optional] To enable logging. It is disabled by default for better performance.
+    'logging' => true,
+ 
+    // [optional]
+    // Error mode
+    // Error handling strategies when the error is occurred.
+    // PDO::ERRMODE_SILENT (default) | PDO::ERRMODE_WARNING | PDO::ERRMODE_EXCEPTION
+    // Read more from https://www.php.net/manual/en/pdo.error-handling.php.
+    'error' => PDO::ERRMODE_SILENT,
+ 
+    // [optional]
+    // The driver_option for connection.
+    // Read more from http://www.php.net/manual/en/pdo.setattribute.php.
+    'option' => [
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ],
+ 
+    // [optional] Medoo will execute those commands after the database is connected.
+    'command' => [
+        'SET SQL_MODE=ANSI_QUOTES'
+    ]
+]);
 
 
     $data = $database->select("test_stundenplan",[
@@ -108,7 +110,7 @@
         echo "<a style='font-size:2em; color: black;' href='../stundenpläne'>Zrück zur Startseite</a>";
 
     }
-    
+
     ?>
 
 <div class="container">
@@ -450,5 +452,6 @@ function dropdown(){
 }
 
 </script>
+
 </body>
 </html>
