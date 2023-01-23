682cf4f98a29eaf5667960af87ec800b209cb681
Bommberk <jim.mohncke@rshw.de>
2023-01-23 15:09:57 +0100

    test3

--- stundenpläne/index.php
+++ stundenpläne/index.php
@@ -9,7 +9,71 @@
 </head>
 <body>
     <?php
+
         echo "hallo was geht bei dir!!!";
+    
+    require("Medoo.php");
+    
+    
+    
+      
+         
+    // Using Medoo namespace.
+    use Medoo\Medoo;
+     
+    $database = new Medoo([
+        // [required]
+        'type' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'stundenplan',
+        'username' => 'root',
+        'password' => '',
+     
+        // [optional]
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_general_ci',
+        'port' => 3306,
+     
+        // [optional] The table prefix. All table names will be prefixed as PREFIX_table.
+        'prefix' => '',
+     
+        // [optional] To enable logging. It is disabled by default for better performance.
+        'logging' => true,
+     
+        // [optional]
+        // Error mode
+        // Error handling strategies when the error is occurred.
+        // PDO::ERRMODE_SILENT (default) | PDO::ERRMODE_WARNING | PDO::ERRMODE_EXCEPTION
+        // Read more from https://www.php.net/manual/en/pdo.error-handling.php.
+        'error' => PDO::ERRMODE_SILENT,
+     
+        // [optional]
+        // The driver_option for connection.
+        // Read more from http://www.php.net/manual/en/pdo.setattribute.php.
+        'option' => [
+            PDO::ATTR_CASE => PDO::CASE_NATURAL
+        ],
+     
+        // [optional] Medoo will execute those commands after the database is connected.
+        'command' => [
+            'SET SQL_MODE=ANSI_QUOTES'
+        ]
+    ]);
+
+
+    $data = $database->select("test_stundenplan",[
+        "id",
+        "tag",
+        "stunde",
+        "was",
+        "wer",
+    ]);
+
+    $data = json_decode(json_encode($data));
+
+    print_r($data);
+    
+
     ?>
 </body>
 </html>
