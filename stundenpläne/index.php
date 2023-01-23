f823408597d0291687ca1578fc1b445cd8149dc9
Bommberk <jim.mohncke@rshw.de>
2023-01-23 15:07:17 +0100

    ffff

--- stundenpläne/index.php
+++ stundenpläne/index.php
@@ -2,9 +2,14 @@
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Stundenpläne</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 <body>
-    <h1>Hallo was geht bei dir!!!</h1>
+    <?php
+        echo "hallo was geht bei dir!!!";
+    ?>
 </body>
 </html>
